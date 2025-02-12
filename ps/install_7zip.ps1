c0c5702a195c20f0b51fd44ec63fe399adf9fc15
Peter Bielik <bjelko@putm09engms08>
2025-02-12 07:27:04 +0000

    hesla asi funguju

--- ps/install_7zip.ps1
+++ ps/install_7zip.ps1
@@ -1,4 +1,3 @@
 Copy-Item -Path '//putm09engms05.tiremes.contiwan.com/ac_Repository/swrep/7zip_msi/' -Destination 'c:\tmp\7zip_msi' -Recurse -Force
-#$pkg = "c:\tmp\7zip_msi\7z2409.msi"
 msiexec /i "c:\tmp\7zip_msi\7z2409.msi" /quiet /norestart
 
